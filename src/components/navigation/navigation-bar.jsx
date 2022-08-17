5c5ea6101070a3d8fde8afeb850e65237e53c1ca
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-08-17 14:58:10 +0100

    login

--- src/components/navigation/navigation-bar.jsx
+++ src/components/navigation/navigation-bar.jsx
@@ -17,19 +17,19 @@ const TopNav = (props) =>{
         <Navbar sticky='top'  expand="lg" variant='dark' className=' p-3 deep-green'>
           <Container fluid>
           
-            <Navbar.Brand href='/'> 725 Cleaning Admin</Navbar.Brand>
+            <Navbar.Brand to='/'> 725 Cleaning Admin</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               
               <Nav className="ms-auto ">
-              <Nav.Link hidden={loggedOut} href="../schedule/">Cleaning Schedule</Nav.Link>
-              <Nav.Link hidden={loggedOut}href="../weekly/">Weekly Sheet</Nav.Link>
+              <Nav.Link hidden={loggedOut} to="/schedule/">Cleaning Schedule</Nav.Link>
+              <Nav.Link hidden={loggedOut} to="/weekly/">Weekly Sheet</Nav.Link>
               <NavDropdown className='mr-2' title="Cleaning Admin" id="basic-nav-dropdown">
-              <NavDropdown.Item disabled={loggedOut} href="../locations/">Location Management</NavDropdown.Item>
-              <NavDropdown.Item disabled={loggedOut} href="../layouts/">Layout Management</NavDropdown.Item>
+              <NavDropdown.Item disabled={loggedOut} to="/locations/">Location Management</NavDropdown.Item>
+              <NavDropdown.Item disabled={loggedOut} to="/layouts/">Layout Management</NavDropdown.Item>
               
               <NavDropdown.Divider />
-              <NavDropdown.Item disabled={loggedOut} href="../calender/">Calender Management</NavDropdown.Item>
+              <NavDropdown.Item disabled={loggedOut} to="/calender/">Calender Management</NavDropdown.Item>
             </NavDropdown>
               </Nav>
              
