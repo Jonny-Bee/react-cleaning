32fa69f379ad4e82e634c17f0ef7ab6ac46a677a
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-08-17 16:53:52 +0100

    login

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import CalenderView from './components/calender-view/calender-view';
 import LayoutView from './components/layout-view/layout-view';
 import LocationView from './components/location-view/location-view';
 import SheduleView from './components/schedule-view/schedule-view';
-import { Routes, Route } from 'react-router';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/home/home';
 import NavigationBar from './components/routes/navigation/navigation.route';
 import WeeklyView from './components/weekly/weekly-view';
-import { Navigate } from 'react-router-dom';
+
 import { UserContext } from './contexts/user-context/user-context';
 import { useContext } from 'react';
 const App = ()=> {
   const {user} = useContext(UserContext);
+  return(
     <Routes>
       <Route path='/' element = {<NavigationBar />}>
           <Route index element={<Home/>}/>
