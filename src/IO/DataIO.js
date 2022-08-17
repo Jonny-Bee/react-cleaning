5d47bf3e68c6050731748f6cfdd243a7ff5f76ff
Jonathan Bartram <jonny.ou@ntlworld.com>
2022-08-17 14:02:57 +0100

    login

--- src/IO/DataIO.js
+++ src/IO/DataIO.js
@@ -32,7 +32,7 @@ export const Login = (req,onLoaded) => {
     try{
         let params = new URLSearchParams(req).toString();
         console.log('loading...')
-        fetch(api_url + 'login/?' + params,{method:'POST'})
+        fetch(api_url + 'login/?' + params,{method:'POST',mode: 'no-cors'})
         .then(res => res.json())
         .then(data => {onLoaded(data)});
     }
